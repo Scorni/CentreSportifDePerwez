b91a8f456ad3856f15936ac5909dbf9c5b1e865c
Scorni <m.scorniciel@students.ephec.be>
2020-06-20 23:32:26 +0200

    NextJs installed & some NextJS test

---
+++ centresportif/pages/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+import Html from "../src/Html";
+import CustomHead from "../src/CustomHead";
+
+export default class Post extends Component{
+  render(){
+    return(
+     
+        <CustomHead />
+     
+    )
+  }
+}
+
