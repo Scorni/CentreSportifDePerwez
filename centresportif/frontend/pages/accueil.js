aac7ca130dbdb937a4cc873a91fa3ad03b5bb72e
Scorni <m.scorniciel@students.ephec.be>
2020-06-29 00:54:41 +0200

    carousel v1

--- centresportif/frontend/pages/accueil.js
+++ centresportif/frontend/pages/accueil.js
@@ -1,10 +1,14 @@
 import React from "react";
+import MyCarousel from "../components/accueil/carousel"
 
 const Accueil = props => (
-    <div>
+    <div style={{maxWidth:"100%"}}>
         <p>
             Voici l'accueil ! Accueil
         </p>
+        <div  style={{width:"50%",  height:"360px" }}>
+        <MyCarousel></MyCarousel>
+        </div>
     </div>
 )
 export default Accueil;
