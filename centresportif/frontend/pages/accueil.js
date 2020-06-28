0995bc5ae3da3e457fdefee83976a8db0bcae2bd
Scorni <m.scorniciel@students.ephec.be>
2020-06-28 23:50:34 +0200

    second test on branch

--- centresportif/frontend/pages/accueil.js
+++ centresportif/frontend/pages/accueil.js
@@ -3,7 +3,7 @@ import React from "react";
 const Accueil = props => (
     <div>
         <p>
-            Voici l'accueil ! Accueil je suis différent
+            Voici l'accueil ! Accueil
         </p>
     </div>
 )
