6295e67ab9706fea2881698009548efad8ddee2c
Scorni <m.scorniciel@students.ephec.be>
2020-07-26 23:23:49 +0200

    little modif on dropdown + start sports

---
+++ centresportif/frontend/pages/sports/Danse.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Sportsindividuel from "../../components/sports/category/SportsIndividuelsListe"
+
+const Sports = props => (
+    <div>    
+                    <Sportsindividuel></Sportsindividuel>
+    </div>
+)
+export default Sports;
