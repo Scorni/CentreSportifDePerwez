e86c7fe922c8a80296e9e36dd5db758a792181a0
Scorni <m.scorniciel@students.ephec.be>
2020-07-28 19:03:29 +0200

    Function unique image done w/ some responsiveness

--- centresportif/frontend/components/sports/category/SportsIndividuelsListe.js
+++ centresportif/frontend/components/sports/category/SportsIndividuelsListe.js
@@ -1,27 +1,14 @@
 import React,{ useRef, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom'
-import { useSpring, animated } from 'react-spring'
+import {CardGenerator} from './generator';
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+var tabs = ["Athlétisme","Course à pieds","Triathlon"]
 
-function Card({info}) {
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 500, friction: 200 } }))
-    return (
-      <animated.div
-        class= {` ${info} defaultCard`}
-        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-        onMouseLeave={() => set({ xys: [0, 0, 1] })}
-        style={{ transform: props.xys.interpolate(trans) }}>
-
-        </animated.div>
-    )
-  }
 const SportIndividuels = (props) => {
     return (
         <div>
-          <Card info={"card"}></Card>
-          <Card info={"card"}></Card>
+          <CardGenerator tabs = {"athletisme.jpg"}></CardGenerator>
+          <CardGenerator tabs = {"Cap"}></CardGenerator>
+
         </div>
  
     );
--- centresportif/frontend/components/sports/category/generator.js
+++ centresportif/frontend/components/sports/category/generator.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import Link from "next/link";
+import { useSpring, animated } from 'react-spring';
+import { isMobile} from 'react-device-detect';
 
-const MyGenerator = (props) => {
-    return (
-    <div >
-        <Container style={{padding:"10px"}} >
-            <Row style={{padding:"15px",boxShadow :"0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)"}}>
-                <Col sm={{ size: 'auto', offset: 3 }}>
-                    <h5>Souhaitez-vous effectuer une réservation?
-                    <Link  href={'/Reservation'}>
-                        <Button style={{marginLeft:"30px"}}>Cliquez ici !</Button>
-                    </Link>
-                    </h5>
-                </Col>
-            </Row>
-        </Container>
-    </div>
- 
-    );
-  }
-  
-  export default MyGenerator;
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+export function CardGenerator({tabs}) {
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 500, friction: 200 } }));
+    if(!isMobile){
+        return(
+            <animated.div
+            key= {tabs}
+            class= {`defaultCard`}
+            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{ transform: props.xys.interpolate(trans) , backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}>
+            
+            </animated.div>
+        )
+    }else{
+        return(
+            <animated.div 
+            key= {tabs}
+            class= {`defaultCard`}
+            style={{backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}/>
+        )
+    }
+}
