811a4294a34e01f8247d02bcde003d031b49e2af
Scorni <m.scorniciel@students.ephec.be>
2020-07-31 18:01:21 +0200

    first jet of title on cards + size done on them

--- centresportif/frontend/components/sports/category/SportsIndividuelsListe.js
+++ centresportif/frontend/components/sports/category/SportsIndividuelsListe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import {CardGenerator} from './Generator';
 import { Container, Row, Col } from 'reactstrap';
 import  useWindowDimensions  from '../../common/WindowsProperties'
@@ -7,32 +7,71 @@ import  useWindowDimensions  from '../../common/WindowsProperties'
 const SportIndividuels = (props) => {
   const {width,height} = useWindowDimensions();
 
-  
-  if(width > 450){
+  useEffect(() => {
+    console.log("it works");
+  })
+  if(width > 1024){
     
     return (
       <Container className="themed-container">
-        <Row className="mx-auto">
-          <Col className =" mt-3" >
-            <CardGenerator tabs = {"athletisme.jpg"} />
+        <Row className="mx-5">
+          <Col className =" mt-3 " sm={{offset : 2}} >
+            <CardGenerator tabs = {"athlétisme.jpg"} title = {"Athlétisme"} />
           </Col>
           <Col className =" mt-3">
-            <CardGenerator tabs = {"athletisme.jpg"} />
-          </Col>
-          <Col className =" mt-3">
-            <CardGenerator tabs = {"athletisme.jpg"} />
+            <CardGenerator tabs = {"boxe.jpg"} title = {"Boxe anglaise"}/>
           </Col>
         </Row>
       </Container>
     )
-  }else{
+  }else if(width > 768){
     return (
-      <div>
-        <p>cheh</p>
-      </div>
+      <Container className="themed-container">
+      <Row className="mx-auto">
+        <Col className =" mt-3" sm={{offset : 2}} >
+          <CardGenerator tabs = {"athlétisme.jpg"} title = {"Athlétisme"}/>
+        </Col>
+        <Col className =" mt-3">
+          <CardGenerator tabs = {"boxe.jpg"} title = {"Boxe anglaise"}/>
+        </Col>
+      </Row>
+    </Container>
+    )
+  }else if(width > 450){
+    return (
+      <Container className="themed-container">
+      <Row className="mx-auto">
+        <Col className =" mt-3" sm={{offset : 2}} >
+          <CardGenerator tabs = {"athlétisme.jpg"} title = {"Athlétisme"}/>
+        </Col>
+        <Col className =" mt-3">
+          <CardGenerator tabs = {"boxe.jpg"} title = {"Boxe anglaise"}/>
+        </Col>
+      </Row>
+    </Container>
+    )
+  }else if(width < 450){
+    return (
+      <Container className="themed-container">
+      <Row className="mx-auto">
+        <Col className =" mt-3" >
+          <CardGenerator tabs = {"athlétisme.jpg"} title = {"Athlétisme"}/>
+        </Col>
+        <Col className =" mt-3">
+          <CardGenerator tabs = {"boxe.jpg"} title = {"Boxe anglaise"}/>
+        </Col>
+      </Row>
+    </Container>
+    )
+  }else if(width){
+    return (
+      <p>
+        salam
+      </p>
     )
   }
 }
+
   export default SportIndividuels;
 
   /** dimenension ==> >,1024,768,450,< */
--- centresportif/frontend/components/sports/category/generator.js
+++ centresportif/frontend/components/sports/category/generator.js
@@ -1,32 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import  useWindowDimensions  from '../../common/WindowsProperties'
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-export function CardGenerator({tabs}) {
+export function CardGenerator({tabs,title}) {
     const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 500, friction: 200 } }));
-
+    useEffect(() => {
+      console.log("it works");
+    })
     const {width,height} = useWindowDimensions();
-    
-    if(width < 1024){
+    console.log(width)
+    if(width > 1024){
         return(
-            <animated.div 
-            key= {tabs}
-            className= {"responsiveCard"}
-            style={{backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
-            />
+          <animated.div
+          key= {tabs}
+          className= {"defaultCard"}
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans) , backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
+          >
+            <div className = {"textAnimatedDiv"}>
+              <h1>{title}</h1>
+            </div>
+            
+          </animated.div>
         )
-      }else{
+      }else if(width > 768){
         return(
-            <animated.div
-            key= {tabs}
-            className= {"defaultCard"}
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-            onMouseLeave={() => set({ xys: [0, 0, 1] })}
-            style={{ transform: props.xys.interpolate(trans) , backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
-            />
+          <animated.div 
+          key= {tabs}
+          className= {"responsiveCardBig"}
+          style={{backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
+          >
+            <div className = {"textAnimatedDiv"}>
+              <h3>{title}</h3>
+            </div>
+          </animated.div>
+        )
+      }else if(width > 450){
+        /** trouver une soluce car les headers ne rentrent pas comme papa dans maman */
+        return(
+          <animated.div 
+          key= {tabs}
+          className= {"responsiveCardNormal"}
+          style={{backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
+          >
+            <div className = {"textAnimatedDiv"}>
+              <h5>{title}</h5>
+            </div>
+          </animated.div>
+        )
+      }else if(width < 450){
+        return(
+          <animated.div 
+          key= {tabs}
+          className= {"responsiveCardTiny"}
+          style={{backgroundImage: "url("+ "../../../static/img/"+tabs +")"}}
+          >
+            <div className = {"textAnimatedDiv"}>
+              <h5>{title}</h5>
+            </div>
+          </animated.div>
+        )
+      }
+      else if(width == 0){
+        return(
+          <p>
+            salam
+          </p>
         )
       }
 }
