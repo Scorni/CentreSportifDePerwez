9eb76e83c588f90e1dfc7b8f27db0aec9d22b559
Scorni <m.scorniciel@students.ephec.be>
2020-08-03 22:49:22 +0200

    Some frondend test

--- centresportif/frontend/components/sports/Infrastructure/Externe.js
+++ centresportif/frontend/components/sports/Infrastructure/Externe.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { MDBContainer,MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView,MDBCarouselCaption } from 'mdbreact';
-import  useWindowDimensions  from '../../common/WindowsProperties'
-
+import  useWindowDimensions  from '../../common/WindowsProperties';
+import { Container, Row, Col } from 'reactstrap';
+import {HeadGenerator} from '../../sports/category/Generator';
 
 
 
@@ -17,37 +15,120 @@ export default function Externe() {
     const {width,height} = useWindowDimensions();
     const useStyles = makeStyles({
         root: {
-          maxWidth: {width},
+          maxWidth: 500,
         },
       });
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-        <CardMedia
-          component="img"
-          height= {height/2}
-          image="../../../static/img/Infrastructure/Externe.jpg"
-          title="Contemplative Reptile"
-        />
-     
-      <CardActionArea>
-        
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Infrastructure externe
-          </Typography>
-          <Typography variant="body1"  component="p">
-            Le centre sportif de Perwez met à votre disposition  :
-            <br/>-1 terrain synthètique
-            <br/>-1 terrain de football en synthétique
-            <br/>-1 piste d'athlétisme
-            <br/>-1 terrain de beach soccer
-            <br/>-2 terrains de beach volley
-            <br/>-1 parcours de street workout et fitness
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+  <div>
+    <HeadGenerator title={"Infrastructure externe"} />
+    <Container className="themed-container">
+      <Row className="mx-auto">
+        <Col className =" mt-3" >
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              
+              image="../../../static/img/Infrastructure/externe.jpg"
+              title=""
+            ></CardMedia>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Terrain synthétique
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  Le centre sportif de Perwez met à votre disposition  :
+                  <br/>-1 terrain synthètique
+                  <br/>-1 terrain de football en synthétique
+                  <br/>-1 piste d'athlétisme
+                  <br/>-1 terrain de beach soccer
+                  <br/>-2 terrains de beach volley
+                  <br/>-1 parcours de street workout et fitness
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+        <Col className =" mt-3">
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              image="../../../static/img/Infrastructure/externe.jpg"
+            />
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Terrain de football en herbe
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  Le centre sportif de Perwez met à votre disposition  :
+                  <br/>-1 terrain synthètique
+                  <br/>-1 terrain de football en synthétique
+                  <br/>-1 piste d'athlétisme
+                  <br/>-1 terrain de beach soccer
+                  <br/>-2 terrains de beach volley
+                  <br/>-1 parcours de street workout et fitness
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+      <Col className =" mt-3" >
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              
+              image="../../../static/img/Infrastructure/externe.jpg"
+              title=""
+            ></CardMedia>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Piste d'Athlétisme
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  Le centre sportif de Perwez met à votre disposition  :
+                  <br/>-1 terrain synthètique
+                  <br/>-1 terrain de football en synthétique
+                  <br/>-1 piste d'athlétisme
+                  <br/>-1 terrain de beach soccer
+                  <br/>-2 terrains de beach volley
+                  <br/>-1 parcours de street workout et fitness
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+        <Col className =" mt-3">
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              image="../../../static/img/Infrastructure/externe.jpg"
+            />
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Terrain de beach soccer
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  Le centre sportif de Perwez met à votre disposition  :
+                  <br/>-1 terrain synthètique
+                  <br/>-1 terrain de football en synthétique
+                  <br/>-1 piste d'athlétisme
+                  <br/>-1 terrain de beach soccer
+                  <br/>-2 terrains de beach volley
+                  <br/>-1 parcours de street workout et fitness
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  </div>
   );
 }
