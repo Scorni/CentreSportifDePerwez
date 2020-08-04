23282dc64929463814c520b12975669fc9fed318
Scorni <m.scorniciel@students.ephec.be>
2020-08-04 23:36:17 +0200

    tiny modification on externe

--- centresportif/frontend/components/sports/Infrastructure/Externe.js
+++ centresportif/frontend/components/sports/Infrastructure/Externe.js
@@ -39,13 +39,10 @@ export default function Externe() {
                   Terrain synthétique
                 </Typography>
                 <Typography variant="body1"  component="p">
-                  Le centre sportif de Perwez met à votre disposition  :
-                  <br/>-1 terrain synthètique
-                  <br/>-1 terrain de football en synthétique
-                  <br/>-1 piste d'athlétisme
-                  <br/>-1 terrain de beach soccer
-                  <br/>-2 terrains de beach volley
-                  <br/>-1 parcours de street workout et fitness
+                  -Constitué de granule de plastique.<br/>
+                  -Dimension standard de terrain de football.<br/> 
+                  -Adapté pour d'autres sports comme le Hockey sur gazon.
+
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -60,23 +57,18 @@ export default function Externe() {
             <CardActionArea>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Terrain de football en herbe
+                  Terrain de Football en herbe
                 </Typography>
                 <Typography variant="body1"  component="p">
-                  Le centre sportif de Perwez met à votre disposition  :
-                  <br/>-1 terrain synthètique
-                  <br/>-1 terrain de football en synthétique
-                  <br/>-1 piste d'athlétisme
-                  <br/>-1 terrain de beach soccer
-                  <br/>-2 terrains de beach volley
-                  <br/>-1 parcours de street workout et fitness
+                  -Constitué d'herbe du désert du Sahara.<br/>
+                  -Dimension standard de terrain de football.<br/>
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </Col>
       </Row>
-      <Row>
+      <Row className="mx-auto">
       <Col className =" mt-3" >
           <Card className={classes.root}>
             <CardMedia
@@ -91,13 +83,11 @@ export default function Externe() {
                 Piste d'Athlétisme
                 </Typography>
                 <Typography variant="body1"  component="p">
-                  Le centre sportif de Perwez met à votre disposition  :
-                  <br/>-1 terrain synthètique
-                  <br/>-1 terrain de football en synthétique
-                  <br/>-1 piste d'athlétisme
-                  <br/>-1 terrain de beach soccer
-                  <br/>-2 terrains de beach volley
-                  <br/>-1 parcours de street workout et fitness
+                  -Piste de 400m constitué d'un revêtement en caoutchouc.<br/>
+                  -Plateau pour lancer du disque,du marteau et du poids.<br/>
+                  -Zone de lancer de javelot.<br/>
+                  -Zone de saut en longueur.<br/>
+                  -Zone de saut en hauteur.<br/>
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -112,16 +102,53 @@ export default function Externe() {
             <CardActionArea>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                Terrain de beach soccer
+                Terrain de Beach Volley
                 </Typography>
                 <Typography variant="body1"  component="p">
-                  Le centre sportif de Perwez met à votre disposition  :
-                  <br/>-1 terrain synthètique
-                  <br/>-1 terrain de football en synthétique
-                  <br/>-1 piste d'athlétisme
-                  <br/>-1 terrain de beach soccer
-                  <br/>-2 terrains de beach volley
-                  <br/>-1 parcours de street workout et fitness
+                  -2 terrains aux normes standard.<br/>
+                  -Sable provenant de la mer du Nord.<br/>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mx-auto">
+      <Col className =" mt-3" >
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              
+              image="../../../static/img/Infrastructure/externe.jpg"
+              title=""
+            ></CardMedia>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Terrain de Beach Soccer
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  -Terrain aux normes standards.<br/>
+                  -Sable provenant du Litoral.<br/>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Col>
+        <Col className =" mt-3">
+          <Card className={classes.root}>
+            <CardMedia
+              component="img"
+              image="../../../static/img/Infrastructure/externe.jpg"
+            />
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Parcours de Street Workout
+                </Typography>
+                <Typography variant="body1"  component="p">
+                  -Barre de tractions,barre de dips,planche pour abdominaux,etc.<br/>
+                  -Engin de réeduction et de mise en forme.<br/>
                 </Typography>
               </CardContent>
             </CardActionArea>
